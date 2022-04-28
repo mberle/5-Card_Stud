3f120e42fcac027514b530d0d92ed79d5d43bd8e
mberle <97688888+mberle@users.noreply.github.com>
2022-04-28 12:21:45 -0400

    2022.28.4.1
    Worked on scoring logic

--- src/Scorer.java
+++ src/Scorer.java
@@ -6,13 +6,13 @@ public interface Scorer {
 
     static int scoreHand(ArrayList<Card> hand)
     {
-        ArrayList<Card> handToScore = hand;
-        boolean isStraight = isStraight(handToScore);
-        boolean isFlush = isFlush(handToScore);
         int score = 0;
-
-
-       return score;
+        if(hand.size() == 5) {
+            ArrayList<Card> handToScore = hand;
+            boolean isStraight = isStraight(handToScore);
+            boolean isFlush = isFlush(handToScore);
+        }
+        return score;
     }
 
     static boolean isStraight(ArrayList<Card> handToScore)
@@ -41,4 +41,91 @@ public interface Scorer {
         }
         return isFlush;
     }
+
+    static boolean isFourOfAKind(ArrayList<Card> handToScore)
+    {
+        boolean isFourKind = false;
+        CardValueCompare valueComp = new CardValueCompare();
+        Collections.sort(handToScore,valueComp);
+        int occOne = 0;
+        int occTwo = 0;
+        for(Card card : handToScore)
+        {
+            if(card.getValue() == handToScore.get(0).getValue())
+            {
+                occOne++;
+            }
+            else if(card.getValue() == handToScore.get(4).getValue())
+            {
+                occTwo++;
+            }
+        }
+        if(occOne == 4 || occTwo == 4)
+        {
+            isFourKind = true;
+        }
+        return isFourKind;
+    }
+
+    static boolean isFullHouse(ArrayList<Card> handToScore)
+    {
+        boolean isFullHouse = false;
+        CardValueCompare valueComp = new CardValueCompare();
+        Collections.sort(handToScore,valueComp);
+        int occOne = 0;
+        int occTwo = 0;
+        for(Card card : handToScore)
+        {
+            if(card.getValue() == handToScore.get(0).getValue())
+            {
+                occOne++;
+            }
+            else if(card.getValue() == handToScore.get(4).getValue())
+            {
+                occTwo++;
+            }
+        }
+        if((occOne == 3 && occTwo == 2) || (occOne == 2 && occTwo == 3))
+        {
+            isFullHouse = true;
+        }
+        return isFullHouse;
+    }
+
+    static boolean isThreeOfAKind(ArrayList<Card> handToScore)
+    {
+        boolean isThreeKind = false;
+        CardValueCompare valueComp = new CardValueCompare();
+        Collections.sort(handToScore,valueComp);
+        if(handToScore.get(2).getValue() == handToScore.get(0).getValue())
+        {
+            isThreeKind = true;
+        }
+        else if(handToScore.get(2).getValue() == handToScore.get(4).getValue())
+        {
+            isThreeKind = true;
+        }
+        else if(handToScore.get(1).getValue() == handToScore.get(3).getValue())
+        {
+            isThreeKind = true;
+        }
+        return isThreeKind;
+    }
+
+    static boolean isTwoPair(ArrayList<Card> handToScore) {
+        boolean isTwoPair = false;
+        CardValueCompare valueComp = new CardValueCompare();
+        Collections.sort(handToScore, valueComp);
+        if (handToScore.get(0).getValue() == handToScore.get(1).getValue()) {
+            if (handToScore.get(2).getValue() == handToScore.get(3).getValue() || handToScore.get(3).getValue() == handToScore.get(4).getValue()) {
+                isTwoPair = true;
+            }
+        } else {
+            if (handToScore.get(1).getValue() == handToScore.get(2).getValue() && handToScore.get(3).getValue() == handToScore.get(4).getValue()) {
+                isTwoPair = true;
+            }
+        }
+        return isTwoPair;
+    }
+
 }
